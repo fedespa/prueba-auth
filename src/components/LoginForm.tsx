fd274a3268d5260f524f4f87f13b77e9403189ff
fedespa <spagnolofederico3@gmail.com>
2025-03-19 17:53:19 -0300

    Añado editor Bangladesh

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { login } from "@/hooks/login";
+import { prueba } from "@/hooks/prueba";
 import { useState } from "react";
 
 function LoginForm() {
@@ -15,37 +16,47 @@ function LoginForm() {
     console.log(data);
   };
 
+  const handleOnClick = async () => {
+    const data = await prueba()
+  }
+
   return (
-    <form className="flex flex-col gap-3 p-8" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="">Email</label>
-        <input
-          type="email"
-          name=""
-          id=""
-          onChange={(e) =>
-            setValue((prevState) => ({ ...prevState, email: e.target.value }))
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="">Password</label>
-        <input
-          type="password"
-          name=""
-          id=""
-          onChange={(e) =>
-            setValue((prevState) => ({
-              ...prevState,
-              password: e.target.value,
-            }))
-          }
-        />
-      </div>
-      <button type="submit" className="border px-6 py-1.5 rounded-xl cursor-pointer">
-        Iniciar sesión
-      </button>
-    </form>
+    <>
+      <form className="flex flex-col gap-3 p-8" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="">Email</label>
+          <input
+            type="email"
+            name=""
+            id=""
+            onChange={(e) =>
+              setValue((prevState) => ({ ...prevState, email: e.target.value }))
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="">Password</label>
+          <input
+            type="password"
+            name=""
+            id=""
+            onChange={(e) =>
+              setValue((prevState) => ({
+                ...prevState,
+                password: e.target.value,
+              }))
+            }
+          />
+        </div>
+        <button
+          type="submit"
+          className="border px-6 py-1.5 rounded-xl cursor-pointer"
+        >
+          Iniciar sesión
+        </button>
+      </form>
+      <button className="border rounded-2xl px-6 py-1.5 mt-5" onClick={handleOnClick}>Prueba</button>
+    </>
   );
 }
 
---
+++ src/hooks/prueba.ts
@@ -0,0 +1,21 @@
+export async function prueba(){
+    try {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/auth/prueba`
+        const res  = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include"
+        });
+        console.log(res)
+
+        const data = await res.json();
+        console.log(data)
+        
+        return data
+    } catch (error) {
+        console.log(error)
+        return { error: "An error ocurred" };
+    }
+}
